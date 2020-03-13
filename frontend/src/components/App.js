fb57f6992e946362d074ddce579b9912a29f20e8
ChadSutherland <csuth1@live.com>
2020-03-13 10:21:22 -0400

    Add header logo and footer

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,30 +2,35 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Layout } from "antd";
 import { MyHeader } from "./MyNavbar";
-import {withRouter} from "react-router";
+import { withRouter } from "react-router";
+import "../App.css";
 
-const {Header, Content, Footer} = Layout;
+const { Header, Content, Footer } = Layout;
 
 const App = () => {
-  const MyHeaderWithRouter = withRouter(props => <MyHeader {...props}/>);
+  const MyHeaderWithRouter = withRouter(props => <MyHeader {...props} />);
   return (
-  <React.Fragment>
-    <Router>
-      <Layout className="layout">
-        <MyHeaderWithRouter />
-        <Content style={{ padding: '20px 50px' }}>
-          <div className="site-layout-content">
-            <p>Something Here</p>
-          </div>
-        </Content>
-        <Footer>
-          <p> Feet </p>
-        </Footer>
-      </Layout>
+    <React.Fragment>
+      <Router>
+        <Layout className="Site">
+          <MyHeaderWithRouter />
+          <Content className="Site-Content" style={{ padding: "0px 50px" }}>
+            <div className="site-layout-content">
+              <p>
+                Something Here<br></br>
+              </p>
+            </div>
+          </Content>
+          <Footer>
+            <p style={{ textAlign: "center" }}>
+              &copy; BHCC Computer Science Exchange 2020
+            </p>
+          </Footer>
+        </Layout>
 
-      <Switch>
-        <Route exact path="/"></Route>
-        {/* <Route
+        <Switch>
+          <Route exact path="/"></Route>
+          {/* <Route
           exact
           path="/accounts"
           render={props => (
@@ -33,9 +38,10 @@ const App = () => {
           )}
         />
         <Route exact path="/details" component={Details} /> */}
-      </Switch>
-    </Router>
-  </React.Fragment>
-)}
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+};
 
 export default App;
--- frontend/src/components/MyNavbar.js
+++ frontend/src/components/MyNavbar.js
@@ -1,24 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Layout, Menu } from "antd";
-import { withRouter } from "react-router";
-
-const { Header, Content, Footer } = Layout;
+const { Header } = Layout;
 
 export const MyHeader = props => {
   const { location } = props;
-  return(
-  <Header>
-    <div className="logo" />
-    <Menu
-      theme="dark"
-      mode="horizontal"
-      selectedKeys = {[location.pathname]}
-      style={{ lineHeight: '64px' }}
-    >
-      <Menu.Item key="/"><Link to="/">Home</Link></Menu.Item>
-      <Menu.Item key="/projects"><Link to="/projects">Projects</Link></Menu.Item>
-    </Menu>
-  </Header>
-)};
+  return (
+    <Header>
+      <div
+        className="Nav-logo"
+        style={{
+          backgroundImage: "url(" + process.env.PUBLIC_URL + "/logo.png)",
+          backgroundPosition: "center",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat"
+        }}
+      ></div>
 
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        selectedKeys={[location.pathname]}
+        style={{ lineHeight: "64px" }}
+      >
+        <Menu.Item key="/">
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="/projects">
+          <Link to="/projects">Projects</Link>
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+};
