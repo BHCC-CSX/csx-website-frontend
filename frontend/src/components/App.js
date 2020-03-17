7796d9ae207c2592ada17923e98679424c32bdbc
ChadSutherland <csuth1@live.com>
2020-03-17 14:11:27 -0400

    Recreate navbar from previous site using react-bootstrap

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,36 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Layout } from "antd";
-import { MyHeader } from "./MyNavbar";
-import { withRouter } from "react-router";
+import { MyNavbar } from "./MyNavbar";
+import Home from "./Home";
+import Projects from "./Projects";
 import "../App.css";
 
 const { Header, Content, Footer } = Layout;
 
-const App = () => {
-  const MyHeaderWithRouter = withRouter(props => <MyHeader {...props} />);
-  return (
-    <React.Fragment>
-      <Router>
-        <Layout className="Site">
-          <MyHeaderWithRouter />
-          <Content className="Site-Content" style={{ padding: "0px 50px" }}>
-            <div className="site-layout-content">
-              <p>
-                Something Here<br></br>
-              </p>
-            </div>
-          </Content>
-          <Footer>
-            <p style={{ textAlign: "center" }}>
-              &copy; BHCC Computer Science Exchange 2020
-            </p>
-          </Footer>
-        </Layout>
-
-        <Switch>
-          <Route exact path="/"></Route>
-          {/* <Route
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        {/* <Route
           exact
           path="/accounts"
           render={props => (
--- frontend/src/components/MyNavbar.js
+++ frontend/src/components/MyNavbar.js
@@ -1,53 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Layout, Menu } from "antd";
-const { Header } = Layout;
+import { Link, NavLink } from "react-router-dom";
+import { Navbar, Nav, Container, NavItem } from "react-bootstrap";
 
-export const MyHeader = props => {
-  const { location } = props;
-  return (
-    <Header>
-      <div
-        className="Nav-logo"
-        style={{
-          backgroundImage: "url(" + process.env.PUBLIC_URL + "/logo.png)",
-          backgroundPosition: "center",
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat"
-        }}
-      ></div>
+export const MyNavbar = () => (
+  <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+    <Container>
+      <Navbar.Brand as={Link} to="/">
+        BHCC Computer Science Exchange
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="">
+          <Nav.Item as={NavLink} exact to="/" className="Navbar-Link">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as={NavLink} exact to="/projects" className="Navbar-Link">
+            <Nav.Link as={Link} to="/projects">
+              Projects
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Nav className="ml-auto navbar-right">
+          <Navbar.Brand href="https://discord.gg/76xbjPA">
+            <Nav.Link
+              className="fab fa-discord Nav-Icon"
+              style={{ padding: "0", fontSize: "30px", margin: "0" }}
+            ></Nav.Link>
+          </Navbar.Brand>
 
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        selectedKeys={[location.pathname]}
-        style={{ lineHeight: "64px" }}
-      >
-        <Menu.Item key="/">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Item key="/projects">
-          <Link to="/projects">Projects</Link>
-        </Menu.Item>
-        <Menu.Item style={{ float: "right" }}>
-          <a
-            href="https://discord.gg/76xbjPA"
-            data-toggle="tooltip"
-            data-placement="bottom"
-            title="Discord"
-          >
-            <i className="fab fa-discord Nav-icon"></i>
-          </a>
-          <a
-            href="https://github.com/BHCC-CSX"
-            data-toggle="tooltip"
-            data-placement="bottom"
-            title="Github"
-          >
-            <i class="fab fa-github-square Nav-icon"></i>
-          </a>
-        </Menu.Item>
-      </Menu>
-    </Header>
-  );
-};
+          <Navbar.Brand href="https://github.com/BHCC-CSX">
+            <Nav.Link
+              className="fab fa-github-square Nav-Icon"
+              style={{ padding: "0", fontSize: "30px", margin: "0" }}
+            ></Nav.Link>
+          </Navbar.Brand>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
