183083ce7c7621a9c4aaa7c3772ad2c6ee726771
ChadSutherland <csuth1@live.com>
2020-03-18 10:41:00 -0400

    Use LinkContainer to avoid DOM nesting errors

--- frontend/src/components/MyNavbar.js
+++ frontend/src/components/MyNavbar.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export const MyNavbar = () => (
   <Navbar bg="dark" variant="dark" expand="sm" fixed="top">
@@ -11,24 +12,24 @@ export const MyNavbar = () => (
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
-          <Nav.Item as={NavLink} exact to="/" className="Navbar-Link">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item as={NavLink} exact to="/projects" className="Navbar-Link">
-            <Nav.Link as={Link} to="/projects">
-              Projects
-            </Nav.Link>
-          </Nav.Item>
+          <LinkContainer exact to="/">
+            <Nav.Item>
+              <Nav.Link href="/">Home</Nav.Link>
+            </Nav.Item>
+          </LinkContainer>
+          <LinkContainer exact to="/projects">
+            <Nav.Item>
+              <Nav.Link href="/projects">Projects</Nav.Link>
+            </Nav.Item>
+          </LinkContainer>
         </Nav>
         <Nav className="ml-auto navbar-right">
           <Navbar.Brand href="https://discord.gg/76xbjPA">
-            <Nav.Link className="fab fa-discord Nav-Icon Discord-Icon"></Nav.Link>
+            <span className="fab fa-discord Nav-Icon Discord-Icon"></span>
           </Navbar.Brand>
 
           <Navbar.Brand href="https://github.com/BHCC-CSX">
-            <Nav.Link className="fab fa-github-square Nav-Icon"></Nav.Link>
+            <span className="fab fa-github-square Nav-Icon"></span>
           </Navbar.Brand>
         </Nav>
       </Navbar.Collapse>
