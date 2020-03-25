3df4799b3087e434266aba25f8ae83caec4fbf34
ChadSutherland <csuth1@live.com>
2020-03-25 16:00:00 -0400

    Add hook for navbar collapse

--- frontend/src/views/WrappedLayout.js
+++ frontend/src/views/WrappedLayout.js
@@ -2,14 +2,22 @@ import React from "react";
 import MyNavbar from "../components/Navbars/MyNavbar";
 import MyFooter from "../components/Footers/MyFooter";
 
-export const Layout = (props) => {
-    return(
-        <>
-            <MyNavbar transparent={props.transparent} scroll={props.scroll ? props.scroll : undefined} />
-            <div className="wrapper">
-                {props.children}
-            </div>
-            <MyFooter />
-        </>
-        );
-}
+export const Layout = props => {
+  React.useEffect(() => {
+    document.body.classList.add("sidebar-collapse");
+    document.documentElement.classList.remove("nav-open");
+    return function cleanup() {
+      document.body.classList.remove("sidebar-collapse");
+    };
+  });
+  return (
+    <>
+      <MyNavbar
+        transparent={props.transparent}
+        scroll={props.scroll ? props.scroll : undefined}
+      />
+      <div className="wrapper">{props.children}</div>
+      <MyFooter />
+    </>
+  );
+};
