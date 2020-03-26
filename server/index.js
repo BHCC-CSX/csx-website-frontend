bd73c4d537f7ce727c823ebf6a7472606571aafa
Col Crunch <rhartnett35@gmail.com>
2020-03-26 17:39:31 -0400

    Use the redirect function for just react requests.

--- server/index.js
+++ server/index.js
@@ -13,14 +13,11 @@ function forceSSL(req, res, next) {
   next();
 }
 
-// Force SSL
-app.use(forceSSL)
-
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 // All remaining requests return the React app, so it can handle routing.
-app.get("*", function(request, response) {
+app.get("*", forceSSL, function(request, response) {
   response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
