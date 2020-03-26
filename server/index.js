4e0b93e6d840981c3d99031fed672d0be5cf7690
Col Crunch <rhartnett35@gmail.com>
2020-03-26 16:31:06 -0400

    Force SSL

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+var forceSSL = require('express-force-ssl');
 const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Force SSL
+app.use(forceSSL)
 
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, "../frontend/build")));
