3af5713b1ab00cfd2a35f3f733bd502e638fed0d
Col Crunch <rhartnett35@gmail.com>
2020-03-26 17:15:51 -0400

    Use http servers to force SSL?

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 var forceSSL = require('express-force-ssl');
 const path = require("path");
+var http = require('http');
+var https = require('https');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+var server = http.createServer(app);
+var secureServer = https.createServer(app);
 
 // Force SSL
 app.use(forceSSL)
@@ -16,6 +21,10 @@ app.get("*", function(request, response) {
   response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
 
-app.listen(PORT, function() {
+
+sercureServer.listen(443, function() {
+  console.error(`Node listening on port 443`);
+});
+server.listen(PORT, function() {
   console.error(`Node listening on port ${PORT}`);
 });
