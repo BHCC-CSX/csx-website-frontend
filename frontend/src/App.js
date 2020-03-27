405d5f9103f0aaebb86148c8986e2a3e73a11a1b
Col Crunch <rhartnett35@gmail.com>
2020-03-26 23:36:36 -0400

    Add 404 handling route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./views/Home";
 import Projects from "./views/Projects";
 import ProjectsDetail from "./views/ProjectsDetail";
+import { NotFound } from "./views/404";
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
             <Route exact path="/" render={props => <Home {...props} />} />
             <Route exact path="/projects" render={props => <Projects {...props} />} />
             <Route path={"/projects/:id"} render={props => <ProjectsDetail {...props} />} />
+            <Route render={props => <NotFound {...props} />} />
           </Route>
         </Switch>
       </BrowserRouter>
