940d1ce8a0025de6b588f8e2e8a1643de9ac7182
ChadSutherland <csuth1@live.com>
2020-03-27 16:55:11 -0400

    Try removing the header check

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 function forceSSL(req, res, next) {
   // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+  if (!req.secure && process.env.NODE_ENV !== "development") {
     console.log(req.get('host'));
     console.log('\n' + req.url);
     return res.redirect('https://' + req.get('host') + req.url);
