4347aa86793f0bc0a38108d727473b11c21b021f
ChadSutherland <csuth1@live.com>
2020-03-27 15:46:29 -0400

    Change router library

--- frontend/src/views/ProjectsDetail.js
+++ frontend/src/views/ProjectsDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Redirect } from "react-router";
+import { Redirect } from "react-router-dom";
 import { Layout } from "./WrappedLayout";
 import DetailContainer from "../components/DetailContainer";
 
