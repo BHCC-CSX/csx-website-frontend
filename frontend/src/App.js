f84cf467681f55b0b24e10f69935d4c3749eca1c
ChadSutherland <csuth1@live.com>
2020-04-08 10:58:42 -0400

    Add route for login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./views/Home";
 import Projects from "./views/Projects";
 import ProjectsDetail from "./views/ProjectsDetail";
+import Login from "./views/Login";
 import { NotFound } from "./views/404";
 import "./App.css";
 
@@ -14,6 +15,7 @@ const App = () => {
             <Route exact path="/" render={props => <Home {...props} />} />
             <Route exact path="/projects" render={props => <Projects {...props} />} />
             <Route path="/projects/:id" render={props => <ProjectsDetail {...props} />} />
+            <Route path="/login" render={props => <Login {...props} />} />
             <Route path="/404" render={props => <NotFound {...props} />} />
             <Route render={props => <NotFound {...props} />} />
         </Switch>
