efb5d1651f61cf071e44bff2d6d23695b1f7adda
ChadSutherland <csuth1@live.com>
2020-04-09 16:55:35 -0400

    Add blog routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./views/Home";
 import Projects from "./views/Projects";
 import ProjectsDetail from "./views/ProjectsDetail";
+import Blog from "./views/Blog";
+import BlogDetail from "./views/BlogDetail";
 import Login from "./views/Login";
 import { NotFound } from "./views/404";
 import "./App.css";
@@ -15,6 +17,8 @@ const App = () => {
             <Route exact path="/" render={props => <Home {...props} />} />
             <Route exact path="/projects" render={props => <Projects {...props} />} />
             <Route path="/projects/:id" render={props => <ProjectsDetail {...props} />} />
+            <Route exact path="/blog" render={props => <Blog {...props} />} />
+            <Route path="/blog/posts/:id" render={props => <BlogDetail {...props} />} />
             <Route path="/login" render={props => <Login {...props} />} />
             <Route path="/404" render={props => <NotFound {...props} />} />
             <Route render={props => <NotFound {...props} />} />
