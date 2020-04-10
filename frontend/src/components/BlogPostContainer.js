48aee46d755bb7115b373e2a54b624275d47c674
ChadSutherland <csuth1@live.com>
2020-04-10 10:25:50 -0400

    Add header and contents to blog post container

--- frontend/src/components/BlogPostContainer.js
+++ frontend/src/components/BlogPostContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Skeleton from "react-loading-skeleton";
 import MyParallaxHeader from "./Headers/MyParallaxHeader";
-import { Row, Col, Button } from "reactstrap";
+import { Row, Col, Button, Container } from "reactstrap";
 
 export default class BlogPostContainer extends Component {
   static propTypes = {
@@ -20,18 +20,49 @@ export default class BlogPostContainer extends Component {
   render() {
     return (
       <>
-        {this.props.blog ? <MyParallaxHeader headerImage={this.props.blog.image} headerText={this.props.blog.title} /> : <Skeleton width="100%" height="400px" />}
+        {this.props.blog ? (
+          <MyParallaxHeader
+            headerImage={this.props.blog.image}
+            headerText={this.props.blog.title}
+          />
+        ) : (
+          <Skeleton width="100%" height="400px" />
+        )}
         <div className="section">
-          <Row>
-            <Col md={12}>
-              <div className="button-container">
-                <Button className="btn-primary btn-round btn-lg">
-                  <i className="now-ui-icons users_circle-08 text_align-left"></i>
-                  {/* {this.props.user.first + " " + this.props.user.last} */}
-                </Button>
-              </div>
-            </Col>
-          </Row>
+          <Container>
+            <Row>
+              <Col md={12}>
+                <div className="button-container">
+                  <Button className="btn-primary btn-round btn-lg mr-1">
+                    <i className="now-ui-icons users_single-02 pr-2"></i>
+                    {this.props.user && (
+                      this.props.user.first_name +
+                      " " +
+                      this.props.user.last_name
+                    )}
+                  </Button>
+
+                  <Button className="btn-info btn-round btn-lg">
+                    <i className="now-ui-icons shopping_tag-content pr-2"></i>
+                    {this.props.category && (
+                      this.props.category.name
+                    )}
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+          <div className="section">
+            <Container>
+              <Row>
+                <Col md={8} className="ml-auto mr-auto">
+                  <p>
+                    {this.props.blog.content || <Skeleton count={10}/>}
+                  </p>
+                </Col>
+              </Row>
+            </Container>
+          </div>
         </div>
       </>
     );
