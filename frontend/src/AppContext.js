6fd6285d31c32fa495c80991cfa957ff24babaaa
ChadSutherland <csuth1@live.com>
2020-05-08 09:22:49 -0400

    Add react context for managing login state

---
+++ frontend/src/AppContext.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+const AppContext = React.createContext();
+
+const getUserFromToken = token => {
+    if (token) {
+        try {
+            return JSON.parse(atob(token.split('.')[1]));
+        } catch (error) {
+        }
+    }
+    return null;
+}
+
+export class AppContextProvider extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            user: {},
+            token: ""
+        }
+    }
+
+    signup = (userInfo) => {
+        return axios.post(process.env.REACT_APP_API_BASE_URL + '/auth/user/', userInfo)
+            .then(response => {
+                const { token } = response.data
+                const user = getUserFromToken(token)
+                this.setState({ token })
+                return axios.get(process.env.REACT_APP_API_BASE_URL + `/auth/user/${user.id}/`)
+            }).then(response => {
+                const { user } = response.data
+                this.setState({ user })
+                return response
+            })
+    }
+
+    // axiosInstance.post('auth/token/obtain/', {
+    //     username,
+    //     password
+    //   }).then(response => {
+    //     if (response.data.error !== 1) {
+    //       axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
+    //       localStorage.setItem('access_token', response.data.access);
+    //       localStorage.setItem('refresh_token', response.data.refresh);
+    //     } else {
+    //       Alert.alert(JSON.stringify(response))
+    //     }
+    //   }).catch(error => {
+    //     console.log(error);
+    //   });
+    
+    render() {
+        return (
+            <AppContext.Provider
+                value={{
+                    signup: this.signup,
+                    ...this.state
+                }}
+            >
+                {this.props.children}
+            </AppContext.Provider>
+        )
+    }
+}
+
+export const withContext = Component => {
+    return props => {
+        return (
+            <AppContext.Consumer>
+                {
+                    globalState => {
+                        return (
+                            <Component
+                                {...globalState}
+                                {...props}
+                            />
+                        )
+                    }
+                }
+            </AppContext.Consumer>
+        )
+    }
+}
