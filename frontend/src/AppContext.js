02b243428537dd0d368dd03fbcd2f916fd282ecf
ChadSutherland <csuth1@live.com>
2020-05-08 13:12:18 -0400

    add login and logout functions to context

--- frontend/src/AppContext.js
+++ frontend/src/AppContext.js
@@ -3,11 +3,12 @@ import axios from "axios";
 
 const AppContext = React.createContext();
 
-const getUserFromToken = token => {
+const decryptToken = token => {
     if (token) {
         try {
             return JSON.parse(atob(token.split('.')[1]));
         } catch (error) {
+            console.log(error)
         }
     }
     return null;
@@ -19,25 +20,58 @@ export class AppContextProvider extends Component {
         super()
         this.state = {
             user: JSON.parse(localStorage.getItem("user")) || {},
-            token: localStorage.getItem("token") || ""
+            access_token: localStorage.getItem("access_token") || "",
+            refresh_token: localStorage.getItem("refresh_token") || "",
         }
     }
 
     signup = (userInfo) => {
         return axios.post(process.env.REACT_APP_API_BASE_URL + '/auth/user/', userInfo)
             .then(response => {
-                const { token } = response.data
-                localStorage.setItem("token", token)
-                this.setState({ token })
+                const { access_token, refresh_token } = response.data
+                localStorage.setItem("access_token", access_token)
+                localStorage.setItem("refresh_token", refresh_token)
+                this.setState({ access_token, refresh_token })
 
-                const user = getUserFromToken(token)
-                return axios.get(process.env.REACT_APP_API_BASE_URL + `/auth/user/${user.id}/`)
+                const tokenData = decryptToken(access_token)
+                return axios.get(process.env.REACT_APP_API_BASE_URL + `/auth/user/${tokenData.user_id}/`)
             }).then(response => {
-                const { user } = response.data
+                const user = response.data
+                console.log(user)
                 localStorage.setItem("user", JSON.stringify(user));
                 this.setState({ user })
                 return response
             })
+    }
+
+    login = (credentials) => {
+        return axios.post(process.env.REACT_APP_API_BASE_URL + '/auth/token/obtain/', credentials)
+            .then(response => {
+                const { access_token, refresh_token } = response.data
+                localStorage.setItem("access_token", access_token)
+                localStorage.setItem("refresh_token", refresh_token)
+                this.setState({ access_token, refresh_token })
+
+                const tokenData = decryptToken(access_token)
+                return axios.get(process.env.REACT_APP_API_BASE_URL + `/auth/user/${tokenData.user_id}/`)
+            }).then(response => {
+                const user = response.data
+                console.log(user)
+                localStorage.setItem("user", JSON.stringify(user));
+                this.setState({ user })
+                return response
+            })
+    }
+
+    logout = () => {
+        localStorage.removeItem("user")
+        localStorage.removeItem("access_token")
+        localStorage.removeItem("refresh_token")
+        this.setState({
+            user: {},
+            access_token: "",
+            refresh_token: ""
+        })
     }
 
     // axiosInstance.post('auth/token/obtain/', {
@@ -45,9 +79,9 @@ export class AppContextProvider extends Component {
     //     password
     //   }).then(response => {
     //     if (response.data.error !== 1) {
-    //       axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
-    //       localStorage.setItem('access_token', response.data.access);
-    //       localStorage.setItem('refresh_token', response.data.refresh);
+    //       axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access_token;
+    //       localStorage.setItem('access_token_token', response.data.access_token);
+    //       localStorage.setItem('refresh_token_token', response.data.refresh_token);
     //     } else {
     //       Alert.alert(JSON.stringify(response))
     //     }
@@ -60,6 +94,8 @@ export class AppContextProvider extends Component {
             <AppContext.Provider
                 value={{
                     signup: this.signup,
+                    login: this.login,
+                    logout: this.logout,
                     ...this.state
                 }}
             >
