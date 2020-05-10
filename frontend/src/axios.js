38d92bb61b618cfaedae1bd8ee048159f7bb72f7
ChadSutherland <csuth1@live.com>
2020-05-10 09:54:17 -0400

    Implement an axios request interceptor to correctly use access token in authenticated API requests

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,10 +4,19 @@ const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 5000,
     headers: {
-        'Authorization': "JWT " + localStorage.getItem('access_token'),
+        //'Authorization': "JWT " + localStorage.getItem('access_token'),
         'Content-Type': 'application/json',
         'accept': 'application/json'
     }
 });
+
+axiosInstance.interceptors.request.use((config) => {
+    const access_token = localStorage.getItem("access_token")
+    //const refresh_token = localStorage.getItem("refresh_token")
+    if (access_token) {
+        config.headers.Authorization = "JWT " + access_token
+    }
+    return config
+})
 
 export default axiosInstance;
