6b8346c8900255684dfa68451d1f9124e0fbe7bd
ChadSutherland <csuth1@live.com>
2020-05-13 19:09:52 -0400

    Change rendering method for 404 svg to avoid Redirect bug

--- frontend/src/views/404.js
+++ frontend/src/views/404.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Layout } from "./WrappedLayout";
 import { Container } from "reactstrap";
+import { ReactComponent as NotFoundImg } from "../assets/img/NotFoundImg.svg";
 
 // https://icons8.com/ouch/illustration/abstract-page-not-found
 
@@ -12,7 +13,7 @@ export class NotFound extends Component {
                 <Layout>
                     <Container className="content-center" style={{marginTop: "80px"}}>
                         <div className="section section-story-overview text-center">
-                            <object type="image/svg+xml" data={"../404.svg"} height={500}>Your browser does not support SVG. :(</object>
+                            <NotFoundImg style={{ maxHeight: "500" }}/>
                             <h1>Oops!</h1>
                             <h2 className={"lead"}>The resource you requested could not be found!</h2>
                         </div>
