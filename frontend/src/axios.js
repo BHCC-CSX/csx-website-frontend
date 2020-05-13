4fcabdc85c41b752d72ea013d41543c2eb6ceace
ChadSutherland <csuth1@live.com>
2020-05-13 11:26:12 -0400

    Remove API timeout

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL,
-    timeout: 5000,
     headers: {
         //'Authorization': "JWT " + localStorage.getItem('access_token'),
         'Content-Type': 'application/json',
